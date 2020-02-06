641b93b2a4a5f1082ac133a6ecc746ea4183be04
Zsolt Biro <zsolt.biro@accenture.com>
2020-02-06 19:06:37 +0200

    Initial project

---
+++ OneTimePassword/OneTimePassword/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneTimePassword
+//
+//  Created by Biro Zsolt on 06/02/2020.
+//  Copyright © 2020 Biro Zsolt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
